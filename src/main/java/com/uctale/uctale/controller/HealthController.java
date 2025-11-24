dc89766d4ce3fb2be422cb5d814e98f29e20776e
krestar <krestar.dev@gmail.com>
2025-11-24 17:32:07 +0900

    fix: Render 배포를 위한 포트 바인딩 설정 및 헬스 체크 엔드포인트 추가

---
+++ src/main/java/com/uctale/uctale/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.uctale.uctale.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String healthCheck() {
+        return "UCTale API is running!";
+    }
+}
