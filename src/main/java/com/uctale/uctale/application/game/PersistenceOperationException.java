71f08751c6d4495917c543eae7910286466148d6
krestar <78223145+krestar@users.noreply.github.com>
2026-08-25 15:52:03 +0900

    fix(api): 일반 영속성 실패를 충돌과 분리

---
+++ src/main/java/com/uctale/uctale/application/game/PersistenceOperationException.java
@@ -0,0 +1,7 @@
+package com.uctale.uctale.application.game;
+
+public class PersistenceOperationException extends RuntimeException {
+    public PersistenceOperationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
