3e0a90b8ccdcf95b2caa1666686b4065a141103a
krestar <78223145+krestar@users.noreply.github.com>
2026-08-30 15:05:19 +0900

    test(security): GameService 비용 제어 의존성 반영

--- src/test/java/com/uctale/uctale/service/GameServiceValidationTest.java
+++ src/test/java/com/uctale/uctale/service/GameServiceValidationTest.java
@@ -1,6 +1,9 @@
 package com.uctale.uctale.service;
 
 import tools.jackson.databind.ObjectMapper;
+import com.uctale.uctale.application.cost.CostRateLimitPolicy;
+import com.uctale.uctale.application.cost.CostRateLimiter;
+import com.uctale.uctale.application.cost.ProviderCallTelemetry;
 import com.uctale.uctale.application.game.ChoiceCodec;
 import com.uctale.uctale.application.game.GamePersistenceService;
 import com.uctale.uctale.application.game.ImagePromptComposer;
@@ -16,6 +19,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.Clock;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -37,12 +41,15 @@ class GameServiceValidationTest {
 
     @BeforeEach
     void setUp() {
+        Clock clock = Clock.systemUTC();
         gameService = new GameService(
                 narrativeGenerator,
                 imageAssetService,
                 gamePersistenceService,
                 new ChoiceCodec(new ObjectMapper()),
-                new ImagePromptComposer()
+                new ImagePromptComposer(),
+                new CostRateLimiter(new CostRateLimitPolicy(1_000, 1_000, 60), clock),
+                new ProviderCallTelemetry(clock, event -> {})
         );
     }
 
@@ -51,9 +58,7 @@ class GameServiceValidationTest {
     void initGame_RejectsOversizedProviderChoiceBeforeSideEffects() {
         GameInitRequest request = new GameInitRequest("세계관", "캐릭터");
         NarrativeTurn invalidTurn = new NarrativeTurn(
-                "제목",
-                "본문",
-                List.of(new NarrativeTurn.Choice(1, "가".repeat(256))),
+                "제목", "본문", List.of(new NarrativeTurn.Choice(1, "가".repeat(256))),
                 new NarrativeTurn.VisualAssets("street", List.of(), List.of())
         );
         given(narrativeGenerator.createOpening("세계관", "캐릭터")).willReturn(invalidTurn);
@@ -70,12 +75,8 @@ class GameServiceValidationTest {
     void initGame_RejectsDuplicateProviderChoiceIds() {
         GameInitRequest request = new GameInitRequest("세계관", "캐릭터");
         NarrativeTurn invalidTurn = new NarrativeTurn(
-                "제목",
-                "본문",
-                List.of(
-                        new NarrativeTurn.Choice(1, "간다"),
-                        new NarrativeTurn.Choice(1, "멈춘다")
-                ),
+                "제목", "본문",
+                List.of(new NarrativeTurn.Choice(1, "간다"), new NarrativeTurn.Choice(1, "멈춘다")),
                 new NarrativeTurn.VisualAssets("", List.of(), List.of())
         );
         given(narrativeGenerator.createOpening("세계관", "캐릭터")).willReturn(invalidTurn);
