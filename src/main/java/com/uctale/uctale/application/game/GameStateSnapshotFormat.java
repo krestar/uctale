0bbc8e9abb477502f0adf4f741b47a3388c5a6fc
krestar <78223145+krestar@users.noreply.github.com>
2026-08-31 01:06:50 +0900

    refactor(game): snapshot version contract 추가

---
+++ src/main/java/com/uctale/uctale/application/game/GameStateSnapshotFormat.java
@@ -0,0 +1,11 @@
+package com.uctale.uctale.application.game;
+
+final class GameStateSnapshotFormat {
+
+    static final int LEGACY_SCHEMA_VERSION = 0;
+    static final int CURRENT_SCHEMA_VERSION = 1;
+    static final int CURRENT_RULESET_VERSION = 1;
+
+    private GameStateSnapshotFormat() {
+    }
+}
